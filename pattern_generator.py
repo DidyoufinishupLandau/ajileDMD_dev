75a4a9ac37b9371bab7eca2e8197dbde531dde0e
p-parkinson <patrick.parkinson@manchester.ac.uk>
2023-08-30 16:52:21 +0100

    Refactor pattern generator to be more relevant to the patterns used.
    Implement rescale to take any image and scale to DMD size.

--- pattern_generator.py
+++ pattern_generator.py
@@ -10,41 +10,35 @@ The goal is to create a full-size image with small data size.
 """
 import collections
 from functools import partial
-import cv2
 import numpy as np
-import pickle
-from typing import List, Dict, Callable
+from typing import Dict, Callable
+from skimage import transform
 
 # Define image sizes
 ajile_conf = collections.namedtuple("ajile_conf", "DMD_IMAGE_HEIGHT_MAX DMD_IMAGE_WIDTH_MAX")
 aj = ajile_conf(1140, 912)
 
 
-def save_image(image: np.array, file_name: str):
-    """Saves an array as a pickle file in the given relative path"""
-    with open("./patterns/" + file_name + ".pickle", "wb") as handle:
-        pickle.dump(image, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-
-def save_image_list(image: List[np.array], file_name: str):
-    """Saves an array as a pickle file in the given relative path"""
-    with open("./patterns/lists/" + file_name + ".pickle", "wb") as handle:
-        pickle.dump(image, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-
 def one_side(side: str = "left") -> np.array:
     """Flip all mirrors to one side """
     if side == "left":
-        np_image = np.ones(shape=(aj.DMD_IMAGE_HEIGHT_MAX, aj.DMD_IMAGE_WIDTH_MAX, 1), dtype=np.uint8) * 255
+        np_image = np.ones(
+            shape=(aj.DMD_IMAGE_HEIGHT_MAX, aj.DMD_IMAGE_WIDTH_MAX, 1),
+            dtype=np.uint8) * 255
     else:
-        np_image = np.zeros(shape=(aj.DMD_IMAGE_HEIGHT_MAX, aj.DMD_IMAGE_WIDTH_MAX, 1), dtype=np.uint8)
+        np_image = np.zeros(
+            shape=(aj.DMD_IMAGE_HEIGHT_MAX, aj.DMD_IMAGE_WIDTH_MAX, 1),
+            dtype=np.uint8)
 
     return np_image
 
 
 def horizontal_50_50() -> np.array:
     """Flip first row of mirrors to left, second row to right..."""
-    np_image = np.zeros(shape=(aj.DMD_IMAGE_HEIGHT_MAX, aj.DMD_IMAGE_WIDTH_MAX, 1), dtype=np.uint8)
+    np_image = np.zeros(
+        shape=(aj.DMD_IMAGE_HEIGHT_MAX, aj.DMD_IMAGE_WIDTH_MAX, 1),
+        dtype=np.uint8)
+    # Flip every second row
     for im in np_image:
         im[::2] = 255
     return np_image
@@ -52,49 +46,60 @@ def horizontal_50_50() -> np.array:
 
 def vertical_50_50() -> np.array:
     """Flip first column of mirrors to left, second column to right..."""
-    np_image = np.ones(shape=(aj.DMD_IMAGE_HEIGHT_MAX, aj.DMD_IMAGE_WIDTH_MAX, 1), dtype=np.uint8) * 255
+    np_image = np.ones(
+        shape=(aj.DMD_IMAGE_HEIGHT_MAX, aj.DMD_IMAGE_WIDTH_MAX, 1),
+        dtype=np.uint8) * 255
+    # Flip every second column
     np_image[::2] = np_image[::2] * 0
     return np_image
 
 
 def checkers() -> np.array:
     # First pixel is 255, second 0, third 255....
-    np_image = np.zeros(shape=(aj.DMD_IMAGE_HEIGHT_MAX, aj.DMD_IMAGE_WIDTH_MAX, 1), dtype=np.uint8)
+    np_image = np.zeros(
+        shape=(aj.DMD_IMAGE_HEIGHT_MAX, aj.DMD_IMAGE_WIDTH_MAX, 1),
+        dtype=np.uint8)
+    # Iterate over each column
     for i in range(len(np_image)):
+        # Iterate over each row
         for j in range(len(np_image[i])):
             if (i + j) % 2 == 0:
                 np_image[i][j] = 255
+
     return np_image
 
 
 def random_50_50() -> np.array:
     # Generate numbers from 0 to 1 sorted as an array (then multiply by 255)
-    np_image = np.random.randint(2, size=(aj.DMD_IMAGE_HEIGHT_MAX, aj.DMD_IMAGE_WIDTH_MAX, 1), dtype=np.uint8) * 255
+    np_image = np.random.randint(2,
+                                 size=(aj.DMD_IMAGE_HEIGHT_MAX, aj.DMD_IMAGE_WIDTH_MAX, 1),
+                                 dtype=np.uint8) * 255
     return np_image
 
 
 def random_1() -> np.array:
-    np_image = np.zeros(shape=(aj.DMD_IMAGE_HEIGHT_MAX, aj.DMD_IMAGE_WIDTH_MAX, 1), dtype=np.uint8)
+    np_image = np.zeros(
+        shape=(aj.DMD_IMAGE_HEIGHT_MAX, aj.DMD_IMAGE_WIDTH_MAX, 1),
+        dtype=np.uint8)
+    # Generate random coordinates
     a = np.random.randint(0, aj.DMD_IMAGE_HEIGHT_MAX)
     b = np.random.randint(0, aj.DMD_IMAGE_WIDTH_MAX)
+    # Set one pixel to 255
     np_image[a][b] = 255
     return np_image
 
 
-def list_a_a(a: int) -> list[np.array]:
-    # Divide DMD into squares of A x A pixels
-    # Create arrays of only one square on
-    # 1140*912/a^2 images in total => ~ 1140*912/a^2 kB of data
-    images: list = []
-    np_image: np.array
-    for col in range(int(aj.DMD_IMAGE_WIDTH_MAX / a)):
-        for wid in range(int(aj.DMD_IMAGE_HEIGHT_MAX / a)):
-            np_image = np.zeros(shape=(aj.DMD_IMAGE_HEIGHT_MAX, aj.DMD_IMAGE_WIDTH_MAX, 1), dtype=np.uint8)
-            cv2.rectangle(np_image, (wid * a, col * a), ((wid + 1) * a - 1, (col + 1) * a - 1), 255)
-            images.append(np_image)
-    return images
+def rescale(input_image: np.array) -> np.array:
+    """Use the skimage transform module to rescale the provided image to the size given by ajile_conf"""
+    # First, quantize input image to either 0 or 255
+    input_image = np.where(input_image > 127, 255, 0)
+    # Then, resize the image
+    rs = transform.resize(input_image, (aj.DMD_IMAGE_HEIGHT_MAX, aj.DMD_IMAGE_WIDTH_MAX), order=0, anti_aliasing=False)
+    # Finally, quantize the image again and return
+    return np.where(rs > 127, 255, 0)
 
 
+# Dictionary of patterns
 named_patterns: Dict[str, Callable] = {
     "one_side_left": partial(one_side, side="left"),
     "one_side_right": partial(one_side, side="right"),
